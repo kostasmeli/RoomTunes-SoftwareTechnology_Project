42f19fe54a51d2f98c78da8a639912a2eeae3478
Panagiotis Syrros <66164503+notis1908@users.noreply.github.com>
2021-06-03 22:37:48 +0300

    Add files via upload

--- Registration.java
+++ Registration.java
@@ -7,26 +7,27 @@ public class Registration {
     public static void main(String[] args)
     {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print(" Name => ");
+            System.out.print("Name => ");
             String Name = scanner.nextLine();
             register.setName(Name);
 
-            System.out.print(" UserName => ");
+            System.out.print("UserName => ");
             String UserName = scanner.nextLine();
             register.setUserName(UserName);
 
-            System.out.print(" Password => ");
+            System.out.print("Password => ");
             String Password = scanner.nextLine();
             register.setPassword(Password);
 
-            System.out.print(" E-Mail => ");
+            System.out.print("E-Mail => ");
             String EMail = scanner.nextLine();
             register.setEMail(EMail);
 
-            System.out.print(" VIP True or False => ");
+            System.out.print("VIP True or False => ");
             Boolean VIPStatus = scanner.nextBoolean();
             register.setVIPStatus(VIPStatus);
         }
+        System.out.println("Registration Successful");
     }
 
 }
