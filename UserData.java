7ff741c543cd542e5720f0d335e9256982833d85
Afroditi Stamatopoulou <75449840+afroditi-stam@users.noreply.github.com>
2021-06-06 00:15:41 +0300

    Add files via upload

--- UserData.java
+++ UserData.java
@@ -1,28 +1,39 @@
-package testing;
-
 import java.util.Scanner;
-public class UserData {
-    public static void main (String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("onoma kai epitheto ");
-        String fullname = sc.nextLine();
-        System.out.println("email ");
-        String email = sc.nextLine();
-        System.out.println("username");
-        String username = sc.nextLine();
-        System.out.println("password ");
-        String password = sc.nextLine();
-        System.out.println("thelete na ginetai VIP? NAI i OXI");
-        String answer = sc.nextLine();
-        boolean VIP = true;
-        // if(answer.contains("NAI")){
-        //   boolean VIP (true);
-        //}
-        //if(answer.contains("OXI"))
-        //  boolean VIP (false);
-        //}
-        System.out.println(fullname);
+public class UserData extends Launcher {
 
+    static Register register = new Register();
 
+    public UserData() {
+        //edw na pairnei apo tin database
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Ta stoixeia sas einai ta eksis:");
+            System.out.println("Onoma:" + register.getName());
+            System.out.println("Ta stoixeia sas einai ta eksis:");
+            System.out.println("Username:" + register.getUserName());
+            System.out.println("Ta stoixeia sas einai ta eksis:");
+            System.out.println("Password:" + register.getPassword());
+            System.out.println("Parakalo grapste poio pedio thelete na allaksete: onoma, email, password, alliws an thelete na vgeite grapste exit ");
+            String pedio = sc.nextLine();
+            while (!pedio.equals("exit") && !pedio.equals("EXIT")) {
+                if (pedio.equals("onoma")) {
+                    System.out.println("Lipoumaste, auto to pedio den mporei na allaksei");
+                    System.out.println("Parakalo grapste poio pedio thelete na allaksete: onoma, email, password, alliws an thelete na vgeite grapste exit ");
+                }
+                if (pedio.equals("email")) {
+                    System.out.println("Dwste neo email");
+                    String EMail = sc.nextLine();
+                    register.setEMail(EMail);
+                    System.out.println("Parakalo grapste poio pedio thelete na allaksete: onoma, email, password, alliws an thelete na vgeite grapste exit ");
+                }
+                if (pedio.equals("password")) {
+                    System.out.println("Dwste neo password");
+                    String Password = sc.nextLine();
+                    register.setPassword(Password);
+                    System.out.println("Parakalo grapste poio pedio thelete na allaksete: onoma, email, password, alliws an thelete na vgeite grapste exit ");
+                }
+                pedio = sc.nextLine();
+            }
+        }
     }
 }
