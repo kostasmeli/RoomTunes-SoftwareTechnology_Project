fc32bc816e2706d88d27de1dba3304b48002aee4
Afroditi Stamatopoulou <75449840+afroditi-stam@users.noreply.github.com>
2021-06-05 18:52:33 +0300

    Add files via upload

--- UserData.java
+++ UserData.java
@@ -3,11 +3,23 @@ package testing;
 import java.util.Scanner;
 public class UserData {
     public void ChangeData (String args[]) {
-        DataBase DBObject = new DataBase()
+        Register register = new Register();
         //edw na pairnei apo tin database
+
         Scanner sc = new Scanner(System.in);
+        System.out.println("Ta stoixeia sas einai ta eksis:");
+        System.out.println("Onoma:" + register.getName());
+        System.out.println("Ta stoixeia sas einai ta eksis:");
+        System.out.println("Username:" + register.getUserName());
+        System.out.println("Ta stoixeia sas einai ta eksis:");
+        System.out.println("Password:" + register.getPassword());
+
+        System.out.println("Parakalo grapste poio pedio thelete na allaksete: Onoma, email, password ");
+        String pedio = sc.nextLine();
+
+
         System.out.println("onoma kai epitheto ");
-        String fullname = sc.nextLine();
+
         System.out.println("email ");
         String email = sc.nextLine();
         System.out.println("username");
